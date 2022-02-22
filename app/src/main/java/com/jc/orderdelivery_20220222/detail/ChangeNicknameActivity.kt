d5bfbdce7f19d0fb48b761b305d8ba3b4ad49e82
JaeCheon <studyJcHub@gmail.com>
2022-02-22 17:49:16 +0900

    내 정보 설정 화면 작성, UI 가 바뀌어 수정  - 닉네임 변경 xml <-> 내 정보 설정 xml

---
+++ app/src/main/java/com/jc/orderdelivery_20220222/detail/ChangeNicknameActivity.kt
@@ -0,0 +1,12 @@
+package com.jc.orderdelivery_20220222.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jc.orderdelivery_20220222.R
+
+class ChangeNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_nickname)
+    }
+}
