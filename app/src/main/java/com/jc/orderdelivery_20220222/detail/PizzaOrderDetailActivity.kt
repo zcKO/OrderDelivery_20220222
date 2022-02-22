59bf403b37c64ad491afe17d7281efe26a3f279e
JaeCheon <studyJcHub@gmail.com>
2022-02-22 14:22:53 +0900

    피자 주문 상세 화면 작성

---
+++ app/src/main/java/com/jc/orderdelivery_20220222/detail/PizzaOrderDetailActivity.kt
@@ -0,0 +1,15 @@
+package com.jc.orderdelivery_20220222.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jc.orderdelivery_20220222.R
+
+class PizzaOrderDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pizza_order_detail)
+
+
+    }
+
+}
